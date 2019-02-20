168b46253c1e58977238d75855421cac642ae58a
Wes Adams <jaada@microsoft.com>
2019-02-20 12:34:23 -0800

    Define Classes and properties for the project

---
+++ PeopleTracker/People.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleTracker
+{
+    enum TypeOfPeople
+    {
+        Client,
+        TeamMember,
+        Friend
+    }
+    class People
+    {
+        #region Properties
+        public string PeopleName { get; set; }
+        public DateTime CreateDate { get; }
+        public string EmailAddress { get; set; }
+        public TypeOfPeople PeopleType { get; set; }
+        public DateTime BirthDate { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public People()
+        {
+            CreateDate = DateTime.UtcNow;
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ PeopleTracker/TrackerApp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleTracker
+{
+    class TrackerApp
+    {
+    }
+}
